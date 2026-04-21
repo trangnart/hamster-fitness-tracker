d514e574f5875ccaf45ef89791f8a4ead3fcf689
Jane Tran <ttran161@ucsc.edu>
2026-04-21 14:04:01 -0700

    switch to polling method

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@ SoftwareSerial mySerial(7, 8);
 //                     RX  TX
 const int HALL_PIN = 2;
 const int LED_PIN = 13;
-const unsigned long DEBOUNCE = 500000; // 500 ms
+const unsigned long DEBOUNCE = 50; // 50 ms
 const unsigned long REPORT_INTERVAL_MS = 1000; // 1000 ms
 
 volatile unsigned long pulseCount = 0;
 volatile unsigned long lastPulse = 0;
-volatile bool magnetDetected = false;
-volatile bool magnetPresent = false;
+bool lastState = HIGH;
+
+static unsigned long ledOnTime = 0;
+static bool ledOn = false;
 
 void setup() {
   Serial.begin(9600);
@@ -19,37 +21,32 @@ void setup() {
   pinMode(HALL_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(HALL_PIN), hallISR, CHANGE); // only count LOW state
-}
-
-void hallISR(){
-  bool currentState = (digitalRead(HALL_PIN) == LOW);
-  unsigned long now = micros();
-
-  if(currentState && !magnetPresent){ // magnet present (OUT -> IN)
-    if(now - lastPulse > DEBOUNCE){
-      pulseCount++;
-      lastPulse = now;
-      magnetDetected = true;
-      magnetPresent = true;
-    }
-  }
-  else if(!currentState){
-    magnetPresent = false;
-  }
+  //attachInterrupt(digitalPinToInterrupt(HALL_PIN), hallISR, CHANGE); // only count LOW state
 }
 
 void loop() {
-  if(magnetDetected){
-    digitalWrite(LED_PIN, HIGH);
-    magnetDetected = false;
-    delay(100); // 100 ms
-    digitalWrite(LED_PIN, LOW);
-  }
   unsigned long nowMS = millis();
   static unsigned long lastCount = 0;
   static unsigned long lastMS = 0;
   const float circumference = 0.3 * 3.14159; // diameter wheel = 30cm
+  bool currentState = digitalRead(HALL_PIN);
+
+  if(lastState == HIGH && currentState == LOW){
+    if(nowMS - lastPulse > DEBOUNCE){
+      pulseCount++;
+      lastPulse = nowMS;
+      digitalWrite(LED_PIN, HIGH);
+      ledOnTime = nowMS;
+      ledOn = true;
+    }
+  }
+  lastState = currentState;
+
+  // turn LED off after 50ms
+  if(ledOn && nowMS - ledOnTime >= DEBOUNCE){
+    digitalWrite(LED_PIN, LOW);
+    ledOn = false;
+  }
 
   if(nowMS - lastMS >= REPORT_INTERVAL_MS){
     noInterrupts();
